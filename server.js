860efce72e87a4b79d9fd31fcf9dcef88337f666
KTDT <dione_3@ymail.com>
2026-02-10 09:55:06 -0500

    Update server.js

--- server.js
+++ server.js
@@ -132,4 +132,8 @@ app.post("/submit", async (req, res) => {
     }
 });
 
-app.listen(3000, () => console.log("Server running on http://localhost:3000"));
+
+const port = process.env.PORT || 3000;
+app.listen(port, "0.0.0.0", () => {
+    console.log(`Server is running on port ${port}`);
+});
